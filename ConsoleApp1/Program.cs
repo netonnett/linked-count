dffc933ecfef5daa2444ca11733423a18bdee714
Szabina Boli <szabina.boli@skola.sala.se>
2024-05-20 10:45:41 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleLinkedList<int> minLista = new SimpleLinkedList<int>();
+            minLista.AddFirst(8643);
+            minLista.AddFirst(3534);
+            minLista.AddFirst(6544);
+            // skriver ut "count: 3"
+            Console.WriteLine("count: " + minLista.Count());
+        }
+
+    }
+}
---
+++ ConsoleApp1/SimpleLinkedList.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp1
+{
+    internal class SimpleLinkedList<T>
+    {
+        
+        public Node<T> First;
+        
+        
+
+        public void AddFirst(T data)
+        {
+            // skapa ett nytt Node-objekt och lägg in värdet data
+            Node<T> newNode = new Node<T>();
+            newNode.Value = data;
+
+            // om det redan finns ett element (nod) inlagd, så
+            // länka den nya noden till den gamla noden
+            if (First != null)
+                newNode.Next = First;
+
+            // sätt listans första nod till att vara den nya noden
+            First = newNode;
+        }
+
+        public int Count()
+        {
+
+            Node<T> minNod = First;
+
+            int numdizzy = 0;
+            while (minNod != null)
+            {
+                Console.WriteLine(minNod.Value);
+                minNod = minNod.Next;
+                numdizzy++;
+            }
+            return numdizzy;
+            
+            
+        }
+
+    }
+}
